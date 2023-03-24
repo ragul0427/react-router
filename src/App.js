3db955102c0fb510d2e547fb0e531b2d83d066c9
ragul0427 <ragulhp2704@gmail.com>
2023-03-24 21:54:44 +0530

    edt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 // import logo from './logo.svg';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import "./App.css";
 import Rootlayouts from "./layouts/rootlayouts";
 import Home from "./screens/home";
@@ -16,60 +11,39 @@ import Phone from "./screens/phone";
 import Pagenotfound from "./screens/pagenotfound";
 import Careerslayout from "./layouts/careerslayout";
 import Careers from "./screens/careers";
-import { careerLoader } from "./screens/careers";
+import {careerLoader} from "./screens/careers";
 import Careererrors from "./screens/careererrors";
 
 
 function App() {
 
-const router = createBrowserRouter(createRoutesFromElements (
-<Route path='/'
-element={<Rootlayouts/>}>
-<Route index
-element={<Home/>}/>
-<Route path='about'
-element={<About/>}/>
-<Route path='contactus'
-element={<Contactlayout/>}>
-<Route path='mail'
-element={<Mailus/>}/>
-<Route path='phone'
-element={<Phone/>}/>
-</Route>
-      <Route path='careers' element={<Careerslayout/>}>
-       <Route index
-element={<Careers/>}/>
-        </Route>
-      <Route path='*' element={<Pagenotfound/>}/>
-</Route>
-))
-return (
-<RouterProvider router={router}/>
+	const router = createBrowserRouter(createRoutesFromElements (
+		<Route path='/'
+			element={<Rootlayouts/>}>
+			<Route index
+				element={<Home/>}/>
+			<Route path='about'
+				element={<About/>}/>
+			<Route path='contactus'
+				element={<Contactlayout/>}>
+				<Route path='mail'
+					element={<Mailus/>}/>
+				<Route path='phone'
+					element={<Phone/>}/>
+			</Route>
+			<Route path='careers'
+				element={<Careerslayout/>}>
+				<Route index
+					element={<Careers/>}/>
+			</Route>
+			<Route path='*'
+				element={<Pagenotfound/>}/>
+		</Route>
+	))
+	return (
+		<RouterProvider router={router}/>
 
-);
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Rootlayouts />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contactus" element={<Contactlayout />}>
-          <Route path="mail" element={<Mailus />} />
-          <Route path="phone" element={<Phone />} />
-        </Route>
-        <Route path="careers" element={<Careerslayout />}>
-          <Route
-            index
-            element={<Careers />}
-            loader={careerLoader}
-            errorElement={<Careererrors />}
-          />
-        </Route>
-        <Route path="*" element={<Pagenotfound />} />
-      </Route>
-    )
-  );
-  return <RouterProvider router={router} />;
+	);
 }
 
 export default App;
-
